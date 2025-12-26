283b817dba967674f2335611b93c56c7b1cf56d3
Thabo1220 <bazaonlineshopping@gmail.com>
2025-12-26 13:48:31 +0200

    Delete backened directory

--- backened/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { makeDeposit } = require('../controllers/depositController');
-
-// POST /api/deposit
-router.post('/', makeDeposit);
-
-module.exports = router;
--- backened/routewithdrawal.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { requestWithdrawal } = require('../controllers/withdrawalController');
-
-// POST /api/withdrawal
-router.post('/', requestWithdrawal);
-
-module.exports = router;
